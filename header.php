36b8521bc6f824c56a3a3130f74d3cd14383c060
Renato Rodrigues Galvão <renato.galvao@mktvirtual.com.br>
2013-06-18 17:20:01 -0300

    Header agora suporta carregamento de CSS direto de qualquer pasta do tema, basta usar  "/_seu_dir". Se nao tiver barra, a funcao ja ira pegar direto da pasta CSS

--- header.php
+++ header.php
@@ -4,15 +4,19 @@
 <!--[if IE 8]>    <html class="no-js lt-ie9" lang="pt_BR"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="pt_BR"> <!--<![endif]-->
 <head>
-<meta charset="utf-8">	
+<?php define('BLOG_TEMPLATE', get_bloginfo('template_directory')) ?>
+<?php define('BLOG_IMAGES', get_bloginfo('template_directory') . '/images') ?>
+<?php define('BLOG_URL', get_bloginfo('url')) ?>
+  
+<meta charset="utf-8">  
 <title><?php wp_title(''); ?></title>
 <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> Feed" href="<?php site_url(); ?>/feed/">
 <meta name="viewport" content="initial-scale=1">
 
 <!--[if lt IE 9]>
-	<script src="<?php echo get_template_directory_uri(); ?>/js/polyfills/html5.js"></script>
-	<script src="<?php echo get_template_directory_uri(); ?>/js/polyfills/selectivizr-min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/polyfills/html5.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/polyfills/selectivizr-min.js"></script>
 <![endif]-->
 
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('template_directory'); ?>/css/formvalidation.css" />
@@ -23,8 +27,16 @@
 <?php global $css; ?>
 <?php if (isset($css)): ?>
   <?php foreach ($css as $style) {
-      if (file_exists(get_dir_path() . '/css/' . $style . '.css')){ ?>
-      <link rel="stylesheet" media="all" href="<?php bloginfo('template_directory'); ?>/css/<?php echo $style; ?>.css" />
+      if ($style[0] == '/') {
+        $filepath = get_dir_path() . $style . '.css';
+        $fileURL = BLOG_TEMPLATE . $style . '.css';
+      }else{
+        $filepath = get_dir_path() . '/css/' . $style . '.css';
+        $fileURL = BLOG_TEMPLATE . '/css/' . $style . '.css';
+      }
+
+      if (file_exists($filepath)){ ?>
+        <link rel="stylesheet" media="all" href="<?php echo $fileURL; ?>" />
       <?php }
     } ?>  
 <?php endif; ?>
@@ -48,6 +60,6 @@
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-	
+  
   
 <div id="root">
